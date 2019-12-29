58dc8379b2fc30cb9635d5b3c278a491a97e08a0
luiscmartinez <mrluismartinezzz@gmail.com>
2019-12-29 14:19:28 -0800

    [working-checkpoint] new /testing is working

---
+++ src/pages/testing.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import React from 'react';
+
+const Testing = () => (
+  <Layout>
+    <h1>DYNAMIC ROUTING FOR FREE</h1>
+  </Layout>
+);
+
+export default Testing;
