91c1f9dfc3d10cfdf4c6a1cdb7d9b6417ea6efa4
luiscmartinez <mrluismartinezzz@gmail.com>
2019-12-29 15:00:20 -0800

    displaying mp4 video

--- src/pages/testing.js
+++ src/pages/testing.js
@@ -1,9 +1,13 @@
 import Layout from '../components/Layout';
 import React from 'react';
-
+import Video from '../../media/joplin.mp4';
 const Testing = () => (
   <Layout>
     <h1>DYNAMIC ROUTING FOR FREE</h1>
+    <video controls width="450">
+      <source type="video/mp4" src={Video} />
+      Sorry, your browser doesn't support embedded videos.
+    </video>
   </Layout>
 );
 
