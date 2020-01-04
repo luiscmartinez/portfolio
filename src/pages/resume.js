58b85420593029a46eb961f336d6509c2e3c4e70
luiscmartinez <mrluismartinezzz@gmail.com>
2020-01-03 19:25:03 -0800

    created resume page

---
+++ src/pages/resume.js
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout';
+import React from 'react';
+import ResumePdf from '../../media/01_03_2019_resume.pdf';
+const Resume = () => (
+  <Layout>
+    <div style={{ border: '2px solid red', height: '100vh' }}>
+      <embed
+        src={ResumePdf}
+        type="application/pdf"
+        width="100%"
+        height="100%"
+      />
+    </div>
+  </Layout>
+);
+
+export default Resume;
