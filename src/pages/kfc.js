a2764b80673aec686b90c9a638a381f2e04f9cf4
luiscmartinez <mrluismartinezzz@gmail.com>
2020-01-06 10:05:41 -0800

    useEffect with fetch

--- src/pages/kfc.js
+++ src/pages/kfc.js
@@ -1,12 +1,32 @@
 import Layout from '../components/Layout';
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-
+import axios from 'axios';
 import clan from '../components/object';
 import { Member } from '../components/Members';
 import { TestingChart } from '../components/clanHelpers';
 const Kfc = () => {
   console.log(clan);
+  useEffect(() => {
+    console.log(process.env.REACT_APP_TOKEN + '');
+    const lol = async () => {
+      console.log('this wat call');
+      const data = await axios
+        .get('https://api.clashofclans.com/v1/clans/?name=kfc', {
+          headers: {
+            Authorization:
+              'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjY2NjgzZGU5LWYzZGEtNDM2NC1iZTc4LTAwMWQ4MWY1MjIyMiIsImlhdCI6MTU3ODMwMzU1MSwic3ViIjoiZGV2ZWxvcGVyLzdhOTU2MDQwLTQwYWQtZWMwYi01MDg0LWY0OGE5MTg2YjhhMiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwOC4xODUuODcuMTM4Il0sInR5cGUiOiJjbGllbnQifV19.VJ2GoXnEMhVxSuxQUpSjShDHxDLNy_SES1ltCAJjY_lGPAnNg8zE8WV2uDHKYibUBXJplRh7ZufdZSUdGmfZIQ',
+          },
+        })
+        .then(function(response) {
+          // handle success
+          console.log(response);
+        })
+        .catch(err => console.error(err));
+      console.log('📦 HERE', data);
+    };
+    lol();
+  }, []);
   return (
     <Layout>
       <div
